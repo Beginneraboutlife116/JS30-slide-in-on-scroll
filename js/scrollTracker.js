9ebcd97c9ac974da511a60eba44c31e92b286be3
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-08-29 15:19:31 +0800

    test popUp

--- js/scrollTracker.js
+++ js/scrollTracker.js
@@ -1,6 +1,7 @@
 import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
 
 const scrollTracker = document.querySelector(".scroll-tracker")
+const popUp = document.querySelector(".pop-up")
 
 scrollTracker.animate(
   {
@@ -9,9 +10,26 @@ scrollTracker.animate(
   {
     duration: 1000,
     timeline: new ScrollTimeline({
-      scrollOffset: [
-        new CSSUnitValue(0, '%'),
-        new CSSUnitValue(100, '%')
+      scrollOffset: [new CSSUnitValue(0, "%"), new CSSUnitValue(100, "%")]
+    })
+  }
+)
+
+popUp.animate(
+  {
+    opacity: "1",
+    clipPath: ["circle(0%)", "circle(125%)"]
+  },
+  {
+    duration: 1,
+    fill: "both",
+    timeline: new ScrollTimeline({
+      scrollSource: document.scrollingElement,
+      timeRange: 1,
+      fill: "both",
+      scrollOffsets: [
+        { target: popUp, edge: "end", threshold: 0.5 },
+        { target: popUp, edge: "end", threshold: 1 }
       ]
     })
   }
